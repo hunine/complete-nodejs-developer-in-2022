24f052c8a1973498545409e4a15aa0e1fc62f617
hqhung201 <hqhung201@gmail.com>
2022-04-03 21:43:59 +0700

    planets project
    - Streaming large data files

---
+++ planets-project/index.js
@@ -0,0 +1,16 @@
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const result = [];
+
+fs.createReadStream('kepler_data.csv')
+    .on('data', (data) => {
+        result.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(result);
+        console.log('done');
+    });
