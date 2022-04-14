a10900e440c1e8e172769b0ae795bf3ae6a8c2c8
Hunine <hqhung201@gmail.com>
2022-04-15 00:11:05 +0700

    Express :tada:

--- 8-express-project/server.js
+++ 8-express-project/server.js
@@ -4,8 +4,39 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+    {
+        id: 0,
+        name: 'hunine',
+    },
+    {
+        id: 1,
+        name: 'bongudth',
+    }, 
+    {
+        id: 2,
+        name: 'koder',
+    }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
+})
+
+app.get('/friends', (req, res) => {
+    res.json(friends);
+})
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if (friend) {
+        res.status(200).json(friend);
+    } else {
+        res.status(404).json({
+            error: 'Friend does not exist'
+        })
+    }
 })
 
 app.get('/messages', (req, res) => {
