591dd1025cb8bd53490ee18203dbdf7e2db478ee
Hunine <hqhung201@gmail.com>
2022-04-15 16:26:36 +0700

    add middlewares

--- 8-express-project/server.js
+++ 8-express-project/server.js
@@ -12,20 +12,27 @@ const friends = [
     {
         id: 1,
         name: 'bongudth',
-    }, 
+    },
     {
         id: 2,
         name: 'koder',
     }
 ];
 
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
-})
+});
 
 app.get('/friends', (req, res) => {
     res.json(friends);
-})
+});
 
 app.get('/friends/:friendId', (req, res) => {
     const friendId = Number(req.params.friendId);
@@ -37,16 +44,16 @@ app.get('/friends/:friendId', (req, res) => {
             error: 'Friend does not exist'
         })
     }
-})
+});
 
 app.get('/messages', (req, res) => {
     res.send('<ul><li>Hello HQH!</li></ul>')
-})
+});
 
 app.post('/messages', (req, res) => {
     console.log('Updating messages...');
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
-})
+});
