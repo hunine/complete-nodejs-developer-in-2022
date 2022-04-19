20cd58b4a17e68dc633112644df164b54c93e45a
Hunine <hqhung201@gmail.com>
2022-04-19 18:12:45 +0700

    Templating Engines

--- 8-express-project/controllers/messages.controller.js
+++ 8-express-project/controllers/messages.controller.js
@@ -1,8 +1,7 @@
 const path = require('path');
 
 function getMessages(req, res) {
-    // res.send('<ul><li>Hello HQH!</li></ul>');
-    res.sendFile(path.join(__dirname, '..', 'public', 'cat.jpg'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'cat.jpg'));
 }
 
 function postMessages(req, res) {
--- 8-express-project/server.js
+++ 8-express-project/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const friendsRouter = require('./routes/friends.router');
 const messagesRouter = require('./routes/messages.router');
 
 const app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 const PORT = 3000;
 
@@ -14,10 +18,14 @@ app.use((req, res, next) => {
     console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.render('index', {
+        title: 'Cute Cat',
+        caption: 'Mew Mew!'
+    });
 });
 
 app.use('/friends', friendsRouter);
