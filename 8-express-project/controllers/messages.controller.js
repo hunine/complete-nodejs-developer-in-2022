99be2014cbc2fd2126071a3a7d078a511556c7bf
Hunine <hqhung201@gmail.com>
2022-04-19 10:24:58 +0700

    Sending Files

--- 8-express-project/controllers/messages.controller.js
+++ 8-express-project/controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Hello HQH!</li></ul>');
+    // res.send('<ul><li>Hello HQH!</li></ul>');
+    res.sendFile(path.join(__dirname, '..', 'public', 'cat.jpg'));
 }
 
 function postMessages(req, res) {
