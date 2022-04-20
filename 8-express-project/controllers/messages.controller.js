750b4d044615fc10c2fb24cec0d9fec2837634ed
Hunine <hqhung201@gmail.com>
2022-04-20 16:05:23 +0700

    Layouts and Separation of Concerns

--- 8-express-project/controllers/messages.controller.js
+++ 8-express-project/controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 function getMessages(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'cat.jpg'));
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'cat.jpg'));
+    res.render('messages', {
+        title: 'Messages to me',
+        friend: 'Hung'
+    })
 }
 
 function postMessages(req, res) {
