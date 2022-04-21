505ee03c85ba5c8f72f52316eb199b2d3332430f
Hunine <hqhung201@gmail.com>
2022-04-21 09:26:12 +0700

    NASA Project
    - GET /planets
    - CORS middleware

--- 9-nasa-project/client/src/hooks/requests.js
+++ 9-nasa-project/client/src/hooks/requests.js
@@ -1,26 +1,25 @@
+const API_URL = 'http://localhost:8000';
+
+// TODO: Once API is ready.
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+    const response = await fetch(`${API_URL}/planets`);
+    return response.json();
 }
 
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+    // TODO: Once API is ready.
+    // Load launches, sort by flight number, and return as JSON.
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+    // TODO: Once API is ready.
+    // Submit given launch data to launch system.
 }
 
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+    // TODO: Once API is ready.
+    // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
--- 9-nasa-project/server/src/app.js
+++ 9-nasa-project/server/src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 app.use(express.json());
+app.use(planetsRouter);
 
 module.exports = app;
