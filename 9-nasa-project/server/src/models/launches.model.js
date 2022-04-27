0d66ee0ab236e2283089e5baae2492ee92109d26
H2Q318 <hqhung201@gmail.com>
2022-04-28 00:38:44 +0700

    NASA Project
    - GET /launches
    - Serving applications with client client side routing
    - Working with data models

--- 9-nasa-project/server/src/models/launches.model.js
+++ 9-nasa-project/server/src/models/launches.model.js
@@ -13,6 +13,10 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
-module.exports = {
-    launches,
+function getAllLaunches() {
+    return Array.from(launches.values());
 }
+
+module.exports = {
+    getAllLaunches,
+};
