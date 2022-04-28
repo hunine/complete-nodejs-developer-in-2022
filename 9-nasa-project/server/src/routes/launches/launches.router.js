38e06d642f07680ee2f4f9a78f363cca12807831
H2Q318 <hqhung201@gmail.com>
2022-04-28 15:15:35 +0700

    POST /launches

--- 9-nasa-project/server/src/routes/launches/launches.router.js
+++ 9-nasa-project/server/src/routes/launches/launches.router.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { httpGetAllLaunches } = require('./launches.controller');
+const {
+    httpGetAllLaunches,
+    httpAddNewLaunches,
+} = require('./launches.controller');
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', httpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunches);
 
 module.exports = launchesRouter;
--- 9-nasa-project/server/src/routes/planets/planets.router.js
+++ 9-nasa-project/server/src/routes/planets/planets.router.js
@@ -2,6 +2,6 @@ const express = require('express');
 const { httpGetAllPlanets } = require('./planets.controller');
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', httpGetAllPlanets);
+planetsRouter.get('/', httpGetAllPlanets);
 
 module.exports = planetsRouter;
