fe677246b702d0e668be54db1b7bdb7638519b8a
H2Q318 <hqhung201@gmail.com>
2022-04-29 17:21:31 +0700

    Rebase

--- 9-nasa-project/client/src/hooks/useLaunches.js
+++ 9-nasa-project/client/src/hooks/useLaunches.js
@@ -15,6 +15,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
         getLaunches();
     }, [getLaunches]);
 
+<<<<<<< HEAD
     const submitLaunch = useCallback(
         async (e) => {
             e.preventDefault();
@@ -45,6 +46,35 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
         },
         [getLaunches, onSuccessSound, onFailureSound]
     );
+=======
+  const submitLaunch = useCallback(async (e) => {
+    e.preventDefault();
+    setPendingLaunch(true);
+    const data = new FormData(e.target);
+    const launchDate = new Date(data.get("launch-day"));
+    const mission = data.get("mission-name");
+    const rocket = data.get("rocket-name");
+    const target = data.get("planets-selector");
+    const response = await httpSubmitLaunch({
+      launchDate,
+      mission,
+      rocket,
+      target,
+    });
+
+    // TODO: Set success based on response.
+    const success = response.ok;
+    if (success) {
+      getLaunches();
+      setTimeout(() => {
+        setPendingLaunch(false);
+        onSuccessSound();
+      }, 800);
+    } else {
+      onFailureSound();
+    }
+  }, [getLaunches, onSuccessSound, onFailureSound]);
+>>>>>>> 34e3215 (Fix bug POST /launches)
 
     const abortLaunch = useCallback(
         async (id) => {
--- 9-nasa-project/server/src/routes/launches/launches.router.js
+++ 9-nasa-project/server/src/routes/launches/launches.router.js
@@ -2,12 +2,18 @@ const express = require('express');
 const {
     httpGetAllLaunches,
     httpAddNewLaunch,
+<<<<<<< HEAD
     httpAbortLaunch
+=======
+>>>>>>> 34e3215 (Fix bug POST /launches)
 } = require('./launches.controller');
 const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
+<<<<<<< HEAD
 launchesRouter.delete('/:id', httpAbortLaunch);
+=======
+>>>>>>> 34e3215 (Fix bug POST /launches)
 
 module.exports = launchesRouter;
