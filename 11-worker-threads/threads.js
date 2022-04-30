cb15341a0b3f1f490a1433280871f9ff1433bba1
H2Q318 <hqhung201@gmail.com>
2022-04-30 13:38:50 +0700

    Worker Threads in action

---
+++ 11-worker-threads/threads.js
@@ -0,0 +1,14 @@
+const { isMainThread, Worker, workerData } = require('worker_threads');
+
+if (isMainThread) {
+    console.log(`Main Thread! Process ID: ${process.pid}`);
+    new Worker(__filename, {
+        workerData: [7, 6, 2, 3],
+    });
+    new Worker(__filename, {
+        workerData: [1, 3, 4, 3],
+    });
+} else {
+    console.log(`Worker Thread! Process ID: ${process.pid}`);
+    console.log(`${workerData} sorted is ${workerData.sort()}`);
+}
