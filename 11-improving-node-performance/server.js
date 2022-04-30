721e7f6c3c32e34fedc46758fbf67916f79028db
H2Q318 <hqhung201@gmail.com>
2022-04-30 10:17:08 +0700

    Improving Node Performance
    - Running Multiple Node Processes
    - Clustering in action

---
+++ 11-improving-node-performance/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cluster = require('cluster');
+const app = express();
+
+function delay(duration) {
+    const startTime = Date.now();
+    while (Date.now() - startTime < duration) {
+        // event loop is blocked...
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send(`Performance example ${process.pid}`);
+});
+
+app.get('/timer', (req, res) => {
+    delay(9000);
+    res.send(`Ding ding ding! ${process.pid}`);
+});
+
+console.log('Running server.js');
+
+if (cluster.isMaster) {
+    console.log('Master has been started...');
+    cluster.fork();
+    cluster.fork();
+} else {
+    console.log('Worker process started...')
+    app.listen(3000);
+}
