55aefa71f27018768f81a0fa8bdbfb36e5a5c6ec
H2Q318 <hqhung201@gmail.com>
2022-05-08 14:05:37 +0700

    Updating API Tests

--- 9-nasa-project/server/src/routes/launches/launches.test.js
+++ 9-nasa-project/server/src/routes/launches/launches.test.js
@@ -14,7 +14,7 @@ describe('Launches API', () => {
     describe('Test GET /launches', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
-                .get('/launches')
+                .get('/v1/launches')
                 .expect('Content-Type', /json/)
                 .expect(200);
         });
@@ -41,7 +41,7 @@ describe('Launches API', () => {
 
         test('It should respond with 201 created', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(completeLaunchData)
                 .expect('Content-Type', /json/)
                 .expect(201);
@@ -56,7 +56,7 @@ describe('Launches API', () => {
 
         test('It should catch missing required properties', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithoutDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
@@ -68,7 +68,7 @@ describe('Launches API', () => {
 
         test('It should catch invalid dates', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithInvalidDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
