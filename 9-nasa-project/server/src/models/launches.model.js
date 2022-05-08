20bfb1396fce3db93a2355f962e6a9e5354d26f6
H2Q318 <hqhung201@gmail.com>
2022-05-08 15:33:32 +0700

    Mapping SpaceX Data to Database

--- 9-nasa-project/server/src/models/launches.model.js
+++ 9-nasa-project/server/src/models/launches.model.js
@@ -1,20 +1,69 @@
+const axios = require('axios');
 const launchesDatabase = require('./launches.mongo');
 const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_FLIGHTS = 100;
 
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    target: 'Kepler-442 b',
-    customers: ['ZTM', 'NASA'],
-    upcoming: true,
-    success: true,
+    flightNumber: 100, //flight_number
+    mission: 'Kepler Exploration X', //name
+    rocket: 'Explorer IS1', //rocket.name
+    launchDate: new Date('December 27, 2030'), //date_local
+    target: 'Kepler-442 b', //not applicable
+    customers: ['ZTM', 'NASA'], //payload.customers
+    upcoming: true, //upcoming
+    success: true, //success
 };
 
 saveLaunch(launch);
+
+const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
+
+async function loadLaunchesData() {
+    console.log('Loading launches data...');
+    try {
+        const response = await axios.post(SPACEX_API_URL, {
+            query: {},
+            options: {
+                populate: [
+                    {
+                        path: 'rocket',
+                        select: {
+                            name: 1,
+                        },
+                    },
+                    {
+                        path: 'payloads',
+                        select: {
+                            customers: 1,
+                        },
+                    },
+                ],
+            },
+        });
+
+        const launchDocs = response.data.docs;
+
+        for (const launchDoc of launchDocs) {
+            const payloads = launchDoc.payloads;
+            const customers = payloads.flatMap((payload) => payload.customers);
+
+            const launch = {
+                flightNumber: launchDoc.flight_number,
+                mission: launchDoc.name,
+                rocket: launchDoc.rocket.name,
+                launchDate: launchDoc.date_local,
+                upcoming: launchDoc.upcoming,
+                success: launchDoc.success,
+                customers,
+            };
+
+            console.log(`${launch.flightNumber} - ${launch.mission}`);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 async function existsLaunchWithId(launchId) {
     return await launchesDatabase.findOne({
@@ -86,6 +135,7 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
+    loadLaunchesData,
     existsLaunchWithId,
     getAllLaunches,
     scheduleNewLaunch,
--- 9-nasa-project/server/src/server.js
+++ 9-nasa-project/server/src/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const app = require('./app');
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchesData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
@@ -11,6 +12,7 @@ const server = http.createServer(app);
 async function startServer() {
     await mongoConnect();
     await loadPlanetsData();
+    await loadLaunchesData();
 
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`);
