de57a2ebffd35d91f3b3a348ba3b23f2bbb5aabc
H2Q318 <hqhung201@gmail.com>
2022-05-09 10:58:31 +0700

    HTTPS With Node
    - Add key.pem, cert.pem

--- 12-security-example/server.js
+++ 12-security-example/server.js
@@ -1,3 +1,5 @@
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
@@ -5,10 +7,19 @@ const PORT = 3000;
 
 const app = express();
 
+app.get('/secret', (req, res) => {
+    res.send('This is secret');
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-});
+https
+    .createServer({
+        key: fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem'),
+    }, app)
+    .listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
